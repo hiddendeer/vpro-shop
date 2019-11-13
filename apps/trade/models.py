97df0f73490f47a1301768937bd1ac422508a084
hiddendeerer <hiddendeerer@gmail.com>
2019-11-13 18:58:48 +0800

    购物车模块 3-4 9

--- apps/trade/models.py
+++ apps/trade/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.db import models
 from django.contrib.auth import get_user_model
 from users.models import UserProfile
@@ -10,6 +11,29 @@ class ShoppingCart(models.Model):
     """
     购物车
     """
-    user = models.ForeignKey(User)
-    goods = models.ForeignKey(Goods)
-    goods_num = models.IntegerField()
+    user = models.ForeignKey(User, verbose_name=u"用户")
+    goods = models.ForeignKey(Goods, verbose_name=u"商品")
+    goods_num = models.IntegerField(default=0, verbose_name="购买数量")
+
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "购物车"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return "%s(%d)".format(self.goods.name, self.goods_num)
+
+
+class OrderInfo(models.Model):
+    """
+    订单
+    """
+    user = models.ForeignKey(User, verbose_name=u"用户")
+    order_sn = models.CharField()
+    trade_no = models.CharField()
+    pay_status = models.CharField()
+    order_mount = models.FloatField()
+    pay_time = models.DateTimeField()
+
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
